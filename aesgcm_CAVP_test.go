4ecb33304782a661d4769abf2f769c6b53e78674
eschorn <eschorn@integritychain.com>
2019-04-29 21:03:57 -0700

    vectors-fu...

--- aesgcm_CAVP_test.go
+++ aesgcm_CAVP_test.go
@@ -5,3 +5,7 @@ package aesgcm_test
 // consider
 //  1. a single test that iterates through the data
 //  2. a single test that only fires test functions
+
+//wget https://csrc.nist.gov/CSRC/media/Projects/Cryptographic-Algorithm-Validation-Program/documents/mac/gcmtestvectors.zip
+//unzip gcmtestvectors.zip -d gcmtestvectors
+//rm gcmtestvectors.zip
